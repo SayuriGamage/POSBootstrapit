e46aac9a7cbe3b872e9aaad99015573d25680c21
Sayuri Gamage <sayuriyashodhara@gmail.com>
2024-10-25 17:17:40 +0530

    customer table update and delete

--- customer.js
+++ customer.js
@@ -3,7 +3,7 @@
 
 let customer_array = [];
 
-// Function to load the customer data into the table
+
 let loadCustomerTable = () => {
     $("#customerTableBody").empty(); // Clear the table before adding new rows
     customer_array.map((customer, index) => {
@@ -20,7 +20,7 @@ let loadCustomerTable = () => {
 };
 
 
-// Save button click event handler
+
 $("#save").on("click", function () {
     console.log("click add customer button");
 
@@ -51,10 +51,10 @@ $("#save").on("click", function () {
     $("#customerForm")[0].reset();
 });
 
-// Variable to store the selected customer's index
+
 let selectedCustomerIndex = null;
 
-// Handle "Select" button click for updating customer
+
 $(document).on("click", ".select-btn", function () {
     selectedCustomerIndex = $(this).data("index");
     const customer = customer_array[selectedCustomerIndex];
@@ -66,7 +66,7 @@ $(document).on("click", ".select-btn", function () {
     $("#address").val(customer.address);
 });
 
-// Update button click event handler
+
 $("#update").on("click", function () {
     console.log("click update customer button");
 
@@ -87,10 +87,9 @@ $("#update").on("click", function () {
 
         console.log("Updated Customer Array: ", customer_array);
 
-        // Reload the customer table with updated data
+
         loadCustomerTable();
 
-        // Reset the form and selected index
         $("#customerForm")[0].reset();
         selectedCustomerIndex = null;
     } else {

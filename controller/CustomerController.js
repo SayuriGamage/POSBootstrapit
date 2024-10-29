b102a54f952a9a5e0371743182358fa36258aaea
Sayuri Gamage <sayuriyashodhara@gmail.com>
2024-10-29 09:56:19 +0530

    update and delete customer table

--- controller/CustomerController.js
+++ controller/CustomerController.js
@@ -32,6 +32,8 @@ const cleanCustomerForm = () => {
 };
 
 
+let selected_customer_index=null;
+
 //add customer
 $("#save").on("click", function () {
     console.log("Save button clicked");
@@ -87,3 +89,133 @@ $("#save").on("click", function () {
        Swal.fire("Success", "Customer added successfully", "success");
    }
 });
+
+$('#customerTableBody').on('click','tr',function (){
+    let index=$(this).index();
+
+    selected_customer_index=$(this).index();
+
+    let customer_obj=customer_array[index];
+
+
+    let full_name=customer_obj.full_name;
+    let mobile=customer_obj.mobile;
+    let email=customer_obj.email;
+    let address =customer_obj.address;
+
+    $('#name').val(full_name);
+    $('#telephone').val(mobile);
+    $('#email').val(email);
+    $('#address').val(address);
+
+});
+
+$('#update').on('click',function (){
+    let index=selected_customer_index;
+
+
+    let full_name=$('#name').val();
+    let mobile=$('#telephone').val();
+    let email=$('#email').val();
+    let address=$('#address').val();
+
+    let customer=new CustomerModel(
+        customer_array[index].id,
+        full_name,
+        mobile,
+        email,
+        address
+    );
+
+    customer_array[selected_customer_index] =customer;
+
+    cleanCustomerForm();
+    loadCustomerTable();
+});
+
+
+$("#delete").on('click',function (){
+    const swalWithBootstrapButtons = Swal.mixin({
+    customClass: {
+        confirmButton: "btn btn-success",
+            cancelButton: "btn btn-danger"
+    },
+    buttonsStyling: false
+});
+swalWithBootstrapButtons.fire({
+    title: "Are you sure?",
+    text: "You won't be able to revert this!",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonText: "Yes, delete it!",
+    cancelButtonText: "No, cancel!",
+    reverseButtons: true
+}).then((result) => {
+    if (result.isConfirmed) {
+
+        // ==========================================================
+        customer_array.splice(selected_customer_index, 1);
+
+        // clean customer form
+        cleanCustomerForm();
+
+        // reload the table
+        loadCustomerTable();
+        // ==========================================================
+
+        swalWithBootstrapButtons.fire({
+            title: "Deleted!",
+            text: "Your customer has been deleted.",
+            icon: "success"
+        });
+    } else if (
+        /* Read more about handling dismissals below */
+        result.dismiss === Swal.DismissReason.cancel
+    ) {
+        swalWithBootstrapButtons.fire({
+            title: "Cancelled",
+            text: "Your imaginary file is safe :)",
+            icon: "error"
+        });
+    }
+});
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
